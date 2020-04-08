705d5509951524e1d04750acc383adf19a95816c
KCMercado <62578555+KCMercado@users.noreply.github.com>
2020-04-08 13:31:49 -0700

    Update traffLightDet_stillImg.py

--- traffLightDet_stillImg.py
+++ traffLightDet_stillImg.py
@@ -3,21 +3,16 @@ import cv2
 import numpy as np
 
 #Read image
-image = cv2.imread('greenlight03.png')
+image = cv2.imread('IMG_0240.JPG')
 
-##kernel_sharpening = np.array([[-1,-1,-1],
-##                              [-1,9,-1],
-##                              [-1,-1,-1]])
-##
-##image = cv2.filter2D(image, -1, kernel_sharpening)
-
+#Define font style
 font = cv2.FONT_HERSHEY_SIMPLEX
     
 #Convert video from BGR to HSV
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
 #Define the lower and upper HSV values for red. Then create the mask for red
-#The mask is a threshold to only get red. However there are two set of range for red
+#The mask is a threshold to only get red. However there are two set of ranges for red
 lower_red1 = np.array([0,180,180])
 upper_red1 = np.array([8,255,255])
 mask_red1 = cv2.inRange(hsv, lower_red1, upper_red1)
@@ -26,6 +21,7 @@ lower_red2 = np.array([170,150,150])
 upper_red2 = np.array([180,255,255])
 mask_red2 = cv2.inRange(hsv, lower_red2, upper_red2)
 
+#Combine red masks
 mask_redComb = mask_red1 + mask_red2
 
 #Define the lower and upper HSV values for yellow. Then create the mask for yellow
@@ -40,7 +36,7 @@ lower_green = np.array([55,111,111])
 upper_green = np.array([100,255,255])
 mask_green = cv2.inRange(hsv, lower_green, upper_green)
 
-#Perform morphological operations to remove any small blobs (noise)
+#Perform morphological operations to remove any small blobs
 mask_redComb = cv2.erode(mask_redComb, None, iterations=1)
 mask_redComb = cv2.dilate(mask_redComb, None, iterations=1)
 
@@ -57,41 +53,41 @@ mask = mask_redComb + mask_yellow + mask_green
 res = cv2.bitwise_and(image, image, mask = mask)
 
 #Hough transform circle detection for all three colors
-r_circles = cv2.HoughCircles(mask_redComb, cv2.HOUGH_GRADIENT, 1, 80, param1=50, param2=8, minRadius=5, maxRadius=50)
-y_circles = cv2.HoughCircles(mask_yellow, cv2.HOUGH_GRADIENT, 1, 80, param1=50, param2=8, minRadius=3, maxRadius=30)
-g_circles = cv2.HoughCircles(mask_green, cv2.HOUGH_GRADIENT, 1, 60, param1=50, param2=10, minRadius=3, maxRadius=30)
+r_circles = cv2.HoughCircles(mask_redComb, cv2.HOUGH_GRADIENT, 1, 80, param1=50, param2=8, minRadius=4, maxRadius=50)
+y_circles = cv2.HoughCircles(mask_yellow, cv2.HOUGH_GRADIENT, 1, 80, param1=50, param2=8, minRadius=3, maxRadius=50)
+g_circles = cv2.HoughCircles(mask_green, cv2.HOUGH_GRADIENT, 1, 60, param1=50, param2=10, minRadius=3, maxRadius=50)
 
+#Label and identify red circles
 if r_circles is not None:
     #Convert the (x,y) coordinates and radius of the circles to integers
     r_circles = np.uint16(np.around(r_circles))
 
     for i in r_circles[0,:]:
-        #Draw the outer circle
+        #Draw the outer circle & label
         cv2.circle(image,(i[0],i[1]),i[2],(255,0,0),1)
-##        cv2.circle(image,(i[0],i[1]),2,(255,0,0),3)
-        cv2.putText(image, 'Red', (i[0],i[1]), font, 1, (255,0,0), 2, cv2.LINE_AA)
-
+        cv2.putText(image, 'Red', (i[0],i[1]), font, 1, (255,0,0), 1, cv2.LINE_AA)
+        
+#Label and identify yellow circles
 if y_circles is not None:
     #Convert the (x,y) coordinates and radius of the circles to integers
     y_circles = np.uint16(np.around(y_circles))
     
     for i in y_circles[0,:]:
-        #Draw the outer circle
+        #Draw the outer circle & label
         cv2.circle(image,(i[0],i[1]),i[2],(225,0,0),1)
-##        cv2.circle(image,(i[0],i[1]),2,(255,0,0),3)
-        cv2.putText(image, 'Yellow', (i[0],i[1]), font, 1, (255,0,0), 2, cv2.LINE_AA)
+        cv2.putText(image, 'Yellow', (i[0],i[1]), font, 1, (255,0,0), 1, cv2.LINE_AA)
 
+#Label and identify green circles
 if g_circles is not None:
     #Convert the (x,y) coordinates and radius of the circles to integers
     g_circles = np.uint16(np.around(g_circles))
     
     for i in g_circles[0,:]:
-        #Draw the outer circle
+        #Draw the outer circle & label
         cv2.circle(image,(i[0],i[1]),i[2],(255,0,0),1)
-##        cv2.circle(image,(i[0],i[1]),2,(255,0,0),3)
-        cv2.putText(image, 'Green', (i[0],i[1]), font, 1, (255,0,0), 2, cv2.LINE_AA)
+        cv2.putText(image, 'Green', (i[0],i[1]), font, 1, (255,0,0), 1, cv2.LINE_AA)
 
-#Show the original, mask, and results
+#Show the original, masks, and results
 cv2.imshow("Original", image)
 cv2.imshow("Mask Red", mask_redComb)
 cv2.imshow("Mask Yellow", mask_yellow)
